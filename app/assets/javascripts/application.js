46975653a256ef834fd4bf4b8bf87cc8869664dc
Juan Pablo Zamora <jupazave@gmail.com>
2014-06-11 21:35:25 -0500

    set up bower

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+// Desde Bower
+//= require bootstrap/dist/js/bootstrap
---
+++ app/controllers/inicio_controller.rb
@@ -0,0 +1,5 @@
+class InicioController < ApplicationController
+	def inicio
+		
+	end
+end
