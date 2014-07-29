e676fb5833683e5674340b3582c5dfe2aac422d9
Juan Pablo Zamora <jupazave@gmail.com>
2014-07-29 12:09:03 -0500

    Added paypal

--- app/admin/fecha.rb
+++ app/admin/fecha.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Fecha do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :title, :price,:link,:city, :date
+  permit_params :title, :price,:link,:city, :date, :paypal
 
   # or
   #
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140720011531) do
+ActiveRecord::Schema.define(version: 20140729163443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20140720011531) do
     t.datetime "updated_at"
     t.string   "city"
     t.string   "date"
+    t.string   "paypal"
   end
 
   create_table "products", force: true do |t|
