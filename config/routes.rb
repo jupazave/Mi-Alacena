5e9dc5ab60d14352bf036b8014273eff592ebbdb
Juan Pablo Zamora <jupazave@gmail.com>
2014-07-29 16:52:50 -0500

    Cambio de una ruta

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'init/index'
-  get 'webhook' => "webhook#index"
+  get 'webhook' => "webhook#push"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
