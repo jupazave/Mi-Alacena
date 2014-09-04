837a0a427f77bfe2503927747c4f98372f966a15
Juan Pablo Zamora <jupazave@gmail.com>
2014-09-04 16:35:16 -0500

    Permite salvar y editar en active admin

--- app/admin/landings_krispykreme.rb
+++ app/admin/landings_krispykreme.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Landings::Krispykreme do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :name, :email, :tel, :glaseadas, :surtidas, :sabores, :pagado, :fecha_envio, :costo
   #
   # or
   #
